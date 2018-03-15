89a2987453a43a21c774dbb4658147b591e68c4c
Sam Bartlett <sab@auburn.edu>
2018-03-14 22:41:22 -0500

    move default shard to const, update header to value from docs

--- pubgy/constants.py
+++ pubgy/constants.py
@@ -1,6 +1,7 @@
 SHARD_LIST = ["xbox-as", "xbox-eu", "xbox-na", "xbox-oc",
                        "pc-krjp", "pc-na", "pc-eu", "pc-oc", "pc-kakao",
                        "pc-sea", "pc-sa", "pc-as"]
+DEFAULT_SHARD = SHARD_LIST[5]
 BASE_URL = "https://api.playbattlegrounds.com/shards/"
 DEBUG_URL = "https://api.playbattlegrounds.com/status"
 SORTS = {"ascending" : "-createdAt", "descending" : "createdAt"}
